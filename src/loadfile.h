c2cfd769c76cd72d9f1b7fc7f5985420fef31d38
marcs-feh <marcosfehlauer@protonmail.com>
2023-05-27 17:39:30 -0300

    renamming

--- src/loadfile.h
+++ src/loadfile.h
@@ -6,17 +6,6 @@
 #include "task.h"
 #include "list.h"
 
-static char* str_slice(const char* s, int begin, int end){
-	if(begin >= end){ return NULL; }
-	int size = end - begin;
-	char* sl = malloc(size + 1);
-	for(int i = begin; i < end; i += 1){
-		sl[i] = s[i];
-	}
-	sl[size] = 0;
-	return sl;
-}
-
 static char* load_file(const char* path){
 	FILE* f = fopen(path, "r");
 	char* data = NULL;
@@ -28,27 +17,13 @@ static char* load_file(const char* path){
 	size = end - start;
 	data = malloc(size + 1);
 	fread(data, 1, size, f);
+	fclose(f);
 	data[size] = 0;
 	return data;
 }
 
 static List parse_tasks(const char* s, int n){
 	List l = list_new();
-
-	int begin = 0;
-	int cur = 0;
-
-	while(cur < n){
-		// Name
-		if(s[cur] == ','){
-			str_slice(s, begin, cur);
-			begin = cur + 1;
-		}
-		// Priority
-
-		// Burst
-		n += 1;
-	}
 	return l;
 }
 
